ab43d900fd82835bd64a9fa91f0741f837409ba5
Nandhini V <nandhini.v.er@gmail.com>
2026-08-26 21:40:21 +0530

    Add src/components/AIRecommendation.jsx

---
+++ src/components/AIRecommendation.jsx
@@ -0,0 +1,4 @@
+import { getCreditAssessment } from "../utils/creditLogic";
+function AIRecommendation({ order }) { const item = getCreditAssessment(order); return <section className={`recommendation ${item.tone}`}><b>âœ¦ {item.action}</b><p>{item.reason}</p></section>; }
+export default AIRecommendation;
+
